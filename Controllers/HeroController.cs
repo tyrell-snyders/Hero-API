c8c606cf866db3167aea1aa0458754ef616ba5a7
tyrell-snyders <tyrell.snyders@gmail.com>
2024-10-22 15:35:01 +0200

    hero controller

---
+++ Controllers/HeroController.cs
@@ -0,0 +1,28 @@
+﻿using Hero_API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hero_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetHeroes()
+        {
+            var heroes = new List<Hero> {
+                new Hero
+                {
+                    Id = 1,
+                    Name = "Spider-Man",
+                    FullName = "Peter",
+                    LastName = "Parker",
+                    Place = "Queens"
+                }
+            };
+
+            return Ok(heroes);
+        }
+    }
+}
