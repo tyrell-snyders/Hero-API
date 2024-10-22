c26d7068349c918299eafddee6407e03af7747b5
tyrell-snyders <tyrell.snyders@gmail.com>
2024-10-22 15:01:59 +0200

    hero entity

---
+++ Entities/Hero.cs
@@ -0,0 +1,11 @@
+﻿namespace Hero_API.Entities
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
